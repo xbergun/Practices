cb94660eb54b2c6ba3eddf8b1131a8b960d8e62f
Berkay <berkay.ergun16@gmail.com>
2022-01-13 02:30:01 +0300

    book added
    book added

---
+++ Book/part1/introduction_js.js
@@ -0,0 +1,15 @@
+function javascript() {
+    var name = "javascript" //! Local variable
+    version = "1.6" //! global variable
+  //? if you dont write a type variable, it is a global variable. Its important for scope.  
+}
+
+
+
+let x;
+
+var a = function f(x) {
+    return 3*x+2;
+} 
+x=2
+console.log(a(x))
